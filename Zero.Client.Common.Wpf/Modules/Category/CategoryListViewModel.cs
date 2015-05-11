3d7a67f419432cbe7800ef1880b500ce8db47920
oceannut <oceannut@163.com>
2015-05-11 23:52:01 +0800

    category list

--- Zero.Client.Common.Wpf/Modules/Category/CategoryListViewModel.cs
+++ Zero.Client.Common.Wpf/Modules/Category/CategoryListViewModel.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 using Caliburn.Micro;
+
+using Nega.WpfCommon;
+
+using Zero.Domain;
+using Zero.BLL;
 
 namespace Zero.Client.Common.Wpf
 {
@@ -12,7 +18,49 @@ namespace Zero.Client.Common.Wpf
     public class CategoryListViewModel : Screen
     {
 
+        private ICategoryService categoryService;
 
+        private ObservableCollection<TreeNodeModel> categoryList;
+
+        public ObservableCollection<TreeNodeModel> CategoryList
+        {
+            get { return categoryList; }
+            set
+            {
+                if (categoryList != value)
+                {
+                    categoryList = value;
+                    NotifyOfPropertyChange(() => CategoryList);
+                }
+            }
+        }
+
+        public CategoryListViewModel(ICategoryService categoryService)
+        {
+            this.categoryService = categoryService;
+        }
+
+        protected override void OnViewLoaded(object view)
+        {
+            base.OnViewLoaded(view);
+
+            this.categoryService.ListCategoryAsync(0)
+                .ContinueWith((task) =>
+                {
+                    if (task.Exception == null)
+                    {
+                        var categories = task.Result;
+                        if (categories != null && categories.Count() > 0)
+                        {
+                            System.Windows.Application.Current.Dispatcher.BeginInvoke(
+                                new System.Action(() =>
+                                {
+                                    this.CategoryList = CategoryViewModel.BuildTree(categories);
+                                }));
+                        }
+                    }
+                });
+        }
 
     }
 
---
+++ Zero.Client.Common.Wpf/Modules/Category/CategoryViewModel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Nega.Common;
+using Nega.WpfCommon;
+
+using Zero.Domain;
+
+namespace Zero.Client.Common.Wpf
+{
+
+    public class CategoryViewModel : TreeNodeModel
+    {
+
+        private Category category;
+        public Category Primitive
+        {
+            get { return category; }
+        }
+
+        public CategoryViewModel(Category category)
+        {
+            if (category == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            this.category = category;
+            this.Name = category.Name;
+        }
+
+        public static ObservableCollection<TreeNodeModel> BuildTree(IEnumerable<Category> col)
+        {
+            ObservableCollection<TreeNodeModel> tree = new ObservableCollection<TreeNodeModel>();
+
+            if (col != null && col.Count() > 0)
+            {
+                List<CategoryViewModel> list = (from item in col
+                                                select new CategoryViewModel(item))
+                                                 .ToList();
+                Queue<TreeNodeModel> queue = new Queue<TreeNodeModel>();
+                foreach (var item in list)
+                {
+                    queue.Enqueue(item);
+                }
+                while (queue.Count > 0)
+                {
+                    CategoryViewModel current = queue.Dequeue() as CategoryViewModel;
+                    if (string.IsNullOrWhiteSpace(current.Primitive.ParentId))
+                    {
+                        tree.Add(current);
+                    }
+                    else
+                    {
+                        TreeNodeModel parent = Tree.Find(tree,
+                            (e) => (e as CategoryViewModel).Primitive.Id == current.Primitive.ParentId) as TreeNodeModel;
+                        if (parent == null)
+                        {
+                            queue.Enqueue(current);
+                        }
+                        else
+                        {
+                            parent.Children.Add(current);
+                        }
+                    }
+                }
+            }
+
+            return tree;
+        }
+
+    }
+
+}
